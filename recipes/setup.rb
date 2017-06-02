664aee4333ee52fb475e845d8a965a86cfdf92cf
brianbroderick <brianbroderick19@gmail.com>
2017-06-02 08:51:31 -0600

    Add defaults

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,7 +1,17 @@
+codename = node['postgresql']['pgdg']['release_apt_codename'] || "xenial"
+version = node['postgresql']['version'] || "9.6"
+
 apt_repository 'apt.postgresql.org' do
  uri 'http://apt.postgresql.org/pub/repos/apt'
- distribution "#{node['postgresql']['pgdg']['release_apt_codename']}-pgdg"
- components ['main', node['postgresql']['version']]
+ distribution "#{codename}-pgdg"
+ components ['main', version]
  key 'https://www.postgresql.org/media/keys/ACCC4CF8.asc'
  action :add
 end
+
+Chef::Log.info("** Installing Postgres **")
+
+package "postgresql-#{version}"
+package "postgresql-client-#{version}"
+package "postgresql-server-dev-#{version}"
+package "postgresql-contrib-#{version}"
