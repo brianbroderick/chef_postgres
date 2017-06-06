596b17060d919c6519e3b646ca3f64daf60fbe94
brianbroderick <brianbroderick19@gmail.com>
2017-06-06 15:49:28 -0600

    Move apt repo to its own recipe

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -4,25 +4,13 @@ first_letter = ['a','b','c','d','e','f'][rand(6)] # has to start with a letter
 admin_user_default = first_letter + ::Digest::MD5.hexdigest(rand.to_s)[0,9]
 admin_pass_default = ::Digest::MD5.hexdigest(rand.to_s)
 
-node.default['postgresql']['pgdg']['release_apt_codename'] = "xenial"
-node.default['postgresql']['version'] = "9.6"
 node.default['postgresql']['admin_login']['username'] = admin_user_default
 node.default['postgresql']['admin_login']['password'] = admin_pass_default
 
-codename = node['postgresql']['pgdg']['release_apt_codename']
-version = node['postgresql']['version']
 admin_user = node['postgresql']['admin_login']['username']
 admin_pass = node['postgresql']['admin_login']['password']
 
-Chef::Log.info("** Setting up apt_repository **")
-
-apt_repository 'apt.postgresql.org' do
- uri 'http://apt.postgresql.org/pub/repos/apt'
- distribution "#{codename}-pgdg"
- components ['main', version]
- key 'https://www.postgresql.org/media/keys/ACCC4CF8.asc'
- action :add
-end
+include_recipe 'chef_postgres::apt_repo'
 
 Chef::Log.info("** Installing Postgres **")
 
@@ -72,9 +60,3 @@ file "Record admin info when using generated info" do
   owner "root"
   path "/etc/postgresql/#{version}/main/admin_login"
 end if admin_pass_default == admin_pass
-
-# Is needed?
-# service "Restart Apparmor" do
-#   action :restart
-#   service_name "apparmor"  
-# end
