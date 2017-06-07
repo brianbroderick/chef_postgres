dbcf52f83d18cb13f6e3d08426bf68dd820054a6
brianbroderick <brianbroderick19@gmail.com>
2017-06-07 09:08:21 -0600

    libraries test

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,7 +1,5 @@
 require "digest"
 
-Hello.world
-
 first_letter = ['a','b','c','d','e','f'][rand(6)] # has to start with a letter
 admin_user_default = first_letter + ::Digest::MD5.hexdigest(rand.to_s)[0,9]
 admin_pass_default = ::Digest::MD5.hexdigest(rand.to_s)
