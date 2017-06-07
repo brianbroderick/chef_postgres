3e2433b475f3e68bebe9c015dcdffb90b957b239
brianbroderick <brianbroderick19@gmail.com>
2017-06-07 11:28:05 -0600

    Use chef dk to add default files for cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,2 +1,9 @@
-name "chef_postgres"
+name 'chef_postgres'
+maintainer 'Brian Broderick'
+maintainer_email 'brianbroderick19 at gmail dot com'
+license 'Apache 2.0'
+description 'Install and configure Postgresql'
+long_description 'Install and configure Postgresql'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
 
