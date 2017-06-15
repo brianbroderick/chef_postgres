23165023bc14198dda6664d49d4968550f3b463d
brianbroderick <brianbroderick19@gmail.com>
2017-06-15 13:52:02 -0600

    Move log output to a separate recipe

---
+++ recipes/log_output.rb
@@ -0,0 +1,36 @@
+### Logging ###
+
+ruby_block 'log_data_directory' do
+  block { ::Chef::Log.info("** Creating Data Directory **") }
+  action :nothing
+end
+
+ruby_block 'log_copy_files' do
+  block { ::Chef::Log.info("** Copying Files **") }
+  action :nothing
+end
+
+ruby_block 'log_stop_pg' do
+  block { ::Chef::Log.info("** Stop Postgres **") }
+  action :nothing
+end
+
+ruby_block 'log_move_data_directory' do
+  block { ::Chef::Log.info("** Moving Data Directory **") }
+  action :nothing
+end
+
+ruby_block 'log_start_pg' do
+  block { ::Chef::Log.info("** Starting Postgres **") }
+  action :nothing
+end
+
+ruby_block 'log_create_admin' do
+  block { ::Chef::Log.info("** Create Admin User **") }
+  action :nothing
+end
+
+ruby_block 'log_create_repl' do
+  block { ::Chef::Log.info("** Create Repl User **") }
+  action :nothing
+end
--- recipes/setup.rb
+++ recipes/setup.rb
@@ -1,3 +1,5 @@
+include_recipe 'postgresql_conf::log_output'
+
 chef_gem 'aws-sdk' do
   compile_time true
 end
@@ -166,39 +168,3 @@ ruby_block 's3_download' do
   end
 end
 
-### Logging ###
-
-ruby_block 'log_data_directory' do
-  block { ::Chef::Log.info("** Creating Data Directory **") }
-  action :nothing
-end
-
-ruby_block 'log_copy_files' do
-  block { ::Chef::Log.info("** Copying Files **") }
-  action :nothing
-end
-
-ruby_block 'log_stop_pg' do
-  block { ::Chef::Log.info("** Stop Postgres **") }
-  action :nothing
-end
-
-ruby_block 'log_move_data_directory' do
-  block { ::Chef::Log.info("** Moving Data Directory **") }
-  action :nothing
-end
-
-ruby_block 'log_start_pg' do
-  block { ::Chef::Log.info("** Starting Postgres **") }
-  action :nothing
-end
-
-ruby_block 'log_create_admin' do
-  block { ::Chef::Log.info("** Create Admin User **") }
-  action :nothing
-end
-
-ruby_block 'log_create_repl' do
-  block { ::Chef::Log.info("** Create Repl User **") }
-  action :nothing
-end
