41094ceebf4f01dd611963493755cd5bd5991d23
brianbroderick <brianbroderick19@gmail.com>
2017-08-31 17:04:51 -0600

    tar C

--- recipes/master.rb
+++ recipes/master.rb
@@ -98,7 +98,7 @@ bash "create_base_backup" do
   code <<-EOF_CBB
   rm -rf #{backup_dir}/base_backup/*
   pg_basebackup -d 'host=localhost user=#{repl_user} password=#{repl_pass}' -D #{backup_dir}/base_backup --xlog-method=stream
-  tar -czfC #{backup_dir}/base_backup.tgz #{backup_dir} base_backup/
+  tar -czfC #{backup_dir}/base_backup.tgz #{backup_dir} #{backup_dir}/base_backup/
   EOF_CBB
   action :run
   notifies :run, "ruby_block[log_create_base_backup]", :before
