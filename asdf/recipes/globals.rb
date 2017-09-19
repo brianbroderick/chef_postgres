dabc655fb85bd7cdacd78f113e2baa3bffca55b8
brianbroderick <brianbroderick19@gmail.com>
2017-09-19 09:54:01 -0600

    Add environment

--- asdf/recipes/globals.rb
+++ asdf/recipes/globals.rb
@@ -1,6 +1,7 @@
 include_recipe "asdf::log_output"
 
 bash "global_erlang" do  
+  environment ({ 'HOME' => ::Dir.home("ubuntu"), 'USER' => "ubuntu" })
   user "ubuntu"
   cwd  "#{node['asdf']['ubuntu_home_dir']}"
   code "#{node["asdf"]["asdf_location"]} global erlang 20.0"
@@ -8,6 +9,7 @@ bash "global_erlang" do
 end
 
 bash "global_elixir" do  
+  environment ({ 'HOME' => ::Dir.home("ubuntu"), 'USER' => "ubuntu" })
   user "ubuntu"
   cwd  "#{node['asdf']['ubuntu_home_dir']}"
   code "#{node["asdf"]["asdf_location"]} global elixir 1.5.1"
@@ -15,6 +17,7 @@ bash "global_elixir" do
 end
 
 bash "global_ruby" do  
+  environment ({ 'HOME' => ::Dir.home("ubuntu"), 'USER' => "ubuntu" })
   user "ubuntu"
   cwd  "#{node['asdf']['ubuntu_home_dir']}"
   code "#{node["asdf"]["asdf_location"]} global ruby 2.4.2"
