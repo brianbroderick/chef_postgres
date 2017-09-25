08689ca63d75aca55196d7bdb18d34a8d90b589e
brianbroderick <brianbroderick19@gmail.com>
2017-09-25 14:48:29 -0600

    Add packages to asdf

--- asdf/attributes/default.rb
+++ asdf/attributes/default.rb
@@ -1,3 +1,5 @@
 default["asdf"]["ubuntu_home_dir"] = "/home/ubuntu"
 default["asdf"]["asdf_location"] = "/home/ubuntu/.asdf/bin/asdf"
 default["asdf"]["aws_region"] = "us-west-2"
+default["asdf"]["version"] = "9.6"
+default["asdf"]["release_apt_codename"] = node["lsb"]["codename"]
--- asdf/recipes/ubuntu_packages.rb
+++ asdf/recipes/ubuntu_packages.rb
@@ -17,3 +17,17 @@ package "libxslt-dev"
 package "m4"
 package "unixodbc-dev"
 package "ruby" # so sudo has ruby available
+
+version = node["asdf"]["version"]
+codename = node["asdf"]["release_apt_codename"]
+
+apt_repository "apt.postgresql.org" do
+  uri "http://apt.postgresql.org/pub/repos/apt"
+  distribution "#{codename}-pgdg"
+  components ["main", version]
+  key "https://www.postgresql.org/media/keys/ACCC4CF8.asc"
+  action :add
+end
+
+package "postgresql-client-common"
+package "postgresql-client-#{version}"
