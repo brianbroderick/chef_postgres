5f4ebf8ee0dd7c7fcefc915fa70891a6864e17c0
brianbroderick <brianbroderick19@gmail.com>
2017-09-27 14:12:00 -0600

    Debugging hex

--- asdf/recipes/install_elixir_tools.rb
+++ asdf/recipes/install_elixir_tools.rb
@@ -3,11 +3,13 @@ include_recipe "asdf::log_output"
 bash "install_hex" do 
   environment ({ 'HOME' => ::Dir.home("ubuntu"), 'USER' => "ubuntu" })
   user "ubuntu"
-  code "/home/ubuntu/.asdf/shims/mix local.hex --force"
+  code ". $HOME/.asdf/asdf.sh
+  /home/ubuntu/.asdf/shims/mix local.hex --force"
   notifies :run, "ruby_block[install_hex]", :before
 end
 
 bash "root_install_hex" do 
-  code "/root/.asdf/shims/mix local.hex --force"
+  code ". $HOME/.asdf/asdf.sh
+  /root/.asdf/shims/mix local.hex --force"
   notifies :run, "ruby_block[install_hex]", :before
 end
