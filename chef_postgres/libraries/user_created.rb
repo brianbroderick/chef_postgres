b0a674a4961e974b1963c0753da38fcd193863ae
brianbroderick <brianbroderick19@gmail.com>
2017-11-16 11:42:11 -0700

    Pass node to UserCreated

--- chef_postgres/libraries/user_created.rb
+++ chef_postgres/libraries/user_created.rb
@@ -1,5 +1,15 @@
 class UserCreated
-  def self.call
+  attr_reader :node
+
+  def self.call(*args)
+    new(*args).call
+  end
+
+  def initialize(node)
+    @node = node
+  end
+
+  def call
     File.exist?(node["chef_postgres"]["vars"]["admin_login_path"])
   end
 end
